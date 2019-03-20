26ca75618344569fb8d8df30213891f8b4ef6f33
Rahul Desai <rd825@georgetown.edu>
2019-03-20 16:46:10 -0400

    Completed ex 1-4

--- ex1/ex1.c
+++ ex1/ex1.c
@@ -9,6 +9,30 @@
 int main(void)
 {
     // Your code here
+    int x = 1;
+    printf("hello world (x: %i)\n", x);
+
+    x = 7;
+
+    int rc = fork();
+
+    // ------------------------------------------------ child process starts executing here
+    if (rc < 0)
+    { // fork failed; exit
+        fprintf(stderr, "fork failed\n");
+        exit(1);
+    }
+    else if (rc == 0)
+    { // child process satisfies this branch
+        x = 23;
+        printf("hello, child here (pid: %d) \n", (int)getpid());
+        printf("updated x: %i\n", x);
+    }
+    else
+    {
+        printf("hello, parent here (pid: %d) of child %d\n", (int)getpid(), rc);
+        printf("updated x: %i\n", x);
+    }
 
     return 0;
 }
--- ex2/ex2.c
+++ ex2/ex2.c
@@ -1,5 +1,5 @@
-// Write a program that opens the text.txt file (with the `fopen()` library call) located in this directory 
-// and then calls `fork()` to create a new process. Can both the child and parent access the file descriptor 
+// Write a program that opens the text.txt file (with the `fopen()` library call) located in this directory
+// and then calls `fork()` to create a new process. Can both the child and parent access the file descriptor
 // returned by `fopen()`? What happens when they are written to the file concurrently?
 
 #include <stdio.h>
@@ -8,7 +8,28 @@
 
 int main(void)
 {
-    // Your code here 
-    
+    // Your code here
+    FILE *fp;
+
+    fp = fopen("text.txt", "w+");
+
+    int rc = fork();
+
+    if (rc < 0)
+    { // fork failed; exit
+        fprintf(stderr, "fork failed\n");
+        exit(1);
+    }
+    else if (rc == 0)
+    { // child process satisfies this branch
+        printf("hello, child here (pid: %d) \n", (int)getpid());
+        fprintf(fp, "This is the parent.");
+    }
+    else
+    {
+        printf("hello, parent here (pid: %d) of child %d\n", (int)getpid(), rc);
+        fprintf(fp, "This is the child.");
+    }
+
     return 0;
 }
